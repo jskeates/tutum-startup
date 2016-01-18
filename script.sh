1a6ff834a33e3a7ade043c2a875ba65b5d82f1b2
Jake Skeates <jake.skeates@ordnancesurvey.co.uk>
2016-01-18 13:55:52 +0000

    Closes #6 - Wait for node to be Deployed
    Fixes #1

--- script.sh
+++ script.sh
@@ -8,6 +8,9 @@
 # API Key: <CREDENTIALS_S3_BUCKET>/<ENVIRONMENT>/tutum_auth_api_key
 CREDENTIALS_S3_BUCKET="xxx"
 ENVIRONMENT="xxx" #e.g. "staging", "production"
+# -- Deployment Timeout: The amount of time to wait for this node to be deployed
+# before the attempt is abandoned
+DEPLOYMENT_TIMEOUT="5m"
 
 # Update instance
 
@@ -44,13 +47,15 @@ tutum node byo | sed -n 4p | source /dev/stdin
 
 tutum node rm $(tutum node list | grep "Unreachable" | awk '{print $1}')
 
-# Sleep until node has deployed
-
-sleep 15
-
 # Set Tutum UUID env var now that tutum-agent has been installed
 
 export TUTUM_UUID=$(cat /etc/tutum/agent/tutum-agent.conf | jq -r .TutumUUID)
+
+# Wait for node to be deployed
+
+echo "Waiting for node to be deployed..."
+timeout $DEPLOYMENT_TIMEOUT bash -c "while [ \"\$(tutum node inspect $TUTUM_UUID | jq -r .state)\" != \"Deployed\" ]; do sleep 10; done;" #TUTUM_UUID is purposefully not escaped
+if [ $? != 0 ]; then echo "Node never came up"; exit 2; fi
 
 # Set Tutum tags based on EC2 tags
 
